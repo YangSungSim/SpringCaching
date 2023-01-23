e9af6f70468113de47bdde93fc6c68367ccf5b25
yangsungsim <yangsungsim227@gmail.com>
2023-01-23 10:55:47 +0900

    error

--- src/main/java/com/example/ReactCaching/service/UserService.java
+++ src/main/java/com/example/ReactCaching/service/UserService.java
@@ -1,6 +1,6 @@
-package com.example.ReactCaching.service;
+package com.example.RedisCaching.service;
 
-import com.example.ReactCaching.dto.UserProfile;
+import com.example.RedisCaching.dto.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -18,20 +18,22 @@ public class UserService {
     StringRedisTemplate redisTemplate;
 
     public UserProfile getUserProfile(String userId) {
+
         String userName = null;
 
         ValueOperations<String, String> ops = redisTemplate.opsForValue();
-        String cachedName = ops.get("nameKey: "+ userId);
-        if(cachedName == null) {
+        String cachedName = ops.get("nameKey:" + userId);
+        if(cachedName != null) {
             userName = cachedName;
-        } else {
-            userName = externalApiService.getUserName(userId);
-            ops.set("nameKey: "+ userId, userName, 5, TimeUnit.SECONDS);
-
         }
+        else {
+            userName = externalApiService.getUserName(userId);
+            ops.set("nameKey:" + userId, userName, 5, TimeUnit.SECONDS);
+        }
+
+        //String userName = externalApiService.getUserName(userId);
         int userAge = externalApiService.getUserAge(userId);
 
         return new UserProfile(userName, userAge);
     }
-
 }
